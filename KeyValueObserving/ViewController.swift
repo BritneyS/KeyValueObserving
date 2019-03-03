ac3dc4e4d8203c1d52c59df865a48ffca5df29ee
Britney Smith <brit.smith7@gmail.com>
2019-03-03 11:33:56 -0500

    Observe name value

--- KeyValueObserving/ViewController.swift
+++ KeyValueObserving/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+class User: NSObject {
+    @objc dynamic var name = String()
+    @objc var age = 0 {
+        willSet{ willChangeValue(forKey: #keyPath(age)) }
+        didSet{ didChangeValue(for: \User.age) }
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var inputTextLabel: UILabel!
@@ -15,19 +23,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     
-    
-    
-    
+    @objc let user = User()
     @objc dynamic var inputText: String?
     var inputTextObservationToken: NSKeyValueObservation?
+    var nameObservationToken: NSKeyValueObservation?
+    var ageObservationToken: NSKeyValueObservation?
+    var indexCount = 0
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        inputTextObservationToken = observe(\ViewController.inputText, options: [.new], changeHandler: { (vc, change) in
+        inputTextObservationToken = observe(\.inputText, options: [.new], changeHandler: { (vc, change) in
             guard let updatedText = change.newValue as? String else { return }
             vc.inputTextLabel.text = updatedText
+        })
+        
+        nameObservationToken = observe(\.user.name, options: [.new], changeHandler: { (vc, change) in
+            guard let updatedNameText = change.newValue else { return }
+            vc.nameLabel.text = updatedNameText
         })
         
     }
@@ -36,6 +50,7 @@ class ViewController: UIViewController {
         super.viewDidDisappear(animated)
         
         inputTextObservationToken?.invalidate()
+        nameObservationToken?.invalidate()
     }
 
 
@@ -44,6 +59,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func updateNameButtonIsTapped(_ sender: UIButton) {
+        let nameArray = ["Maria", "Jade", "Christina", "Ash"]
+            user.name = nameArray[indexCount]
+        if indexCount < nameArray.count - 1 {
+            indexCount += 1
+        } else {
+            indexCount = 0
+        }
     }
     
     @IBAction func updateAgeButtonIsTapped(_ sender: UIButton) {
